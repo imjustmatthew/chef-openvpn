cc7917118c640c4b64ce50f485190003471e71e2
Matthew Roy <mroy@sandbox-ed.com>
2013-05-26 23:45:30 -0400

    Add initial support for encrypted databags

--- recipes/client.rb
+++ recipes/client.rb
@@ -19,40 +19,71 @@ configurtions.each do |config_name,config|
 
   # user_name required for given vpn server/config
   user_name = config[:user_name]
-
-  cookbook_file "/etc/openvpn/#{config_name}-#{user_name}-ca.crt" do
-    source "#{config_name}-ca.crt"
-    owner "root"
-    group "openvpn"
-    mode 00660
-    cookbook config[:file_cookbook] if config[:file_cookbook]
-  end
-
-  if (config[:auth][:type] == "cert") or (config[:auth][:type] == "cert_passwd")
-    cookbook_file "/etc/openvpn/#{config_name}-#{user_name}.crt" do
-      source "#{config_name}-#{user_name}.crt"
+  
+  #First, try loading the crypto materials from an encrypted databag
+  data_bag_item = Chef::EncryptedDataBagItem.load("openvpn-"+config_name, user_name)
+  if data_bag_item[:ca]
+    file "/etc/openvpn/#{config_name}-#{user_name}-ca.crt" do
+      content data_bag_item[:ca]
+      owner "root"
+      group "openvpn"
+      mode 00660
+    end
+    if (config[:auth][:type] == "cert") or (config[:auth][:type] == "cert_passwd")
+      file "/etc/openvpn/#{config_name}-#{user_name}.crt" do
+        content data_bag_item[:cert]
+        owner "root"
+        group "openvpn"
+        mode 00660
+      end
+      file "/etc/openvpn/#{config_name}-#{user_name}.key" do
+        content data_bag_item[:key]
+        owner "root"
+        group "openvpn"
+        mode 00660
+      end
+    end
+    file "/etc/openvpn/#{config_name}-#{user_name}.conf" do
+      content data_bag_item[:conf]
+      owner "root"
+      group "openvpn"
+      mode 00660
+    end
+    
+  else #No data bag? Use files sourced from this cookbook or the specified provider
+    cookbook_file "/etc/openvpn/#{config_name}-#{user_name}-ca.crt" do
+      source "#{config_name}-ca.crt"
       owner "root"
       group "openvpn"
       mode 00660
       cookbook config[:file_cookbook] if config[:file_cookbook]
     end
 
-    cookbook_file "/etc/openvpn/#{config_name}-#{user_name}.key" do
-      source "#{config_name}-#{user_name}.key"
+    if (config[:auth][:type] == "cert") or (config[:auth][:type] == "cert_passwd")
+      cookbook_file "/etc/openvpn/#{config_name}-#{user_name}.crt" do
+        source "#{config_name}-#{user_name}.crt"
+        owner "root"
+        group "openvpn"
+        mode 00660
+        cookbook config[:file_cookbook] if config[:file_cookbook]
+      end
+
+      cookbook_file "/etc/openvpn/#{config_name}-#{user_name}.key" do
+        source "#{config_name}-#{user_name}.key"
+        owner "root"
+        group "openvpn"
+        mode 00660
+        cookbook config[:file_cookbook] if config[:file_cookbook]
+      end
+    end
+
+    cookbook_file "/etc/openvpn/#{config_name}-#{user_name}.conf" do
+      source "#{config_name}-#{user_name}.conf"
       owner "root"
       group "openvpn"
       mode 00660
       cookbook config[:file_cookbook] if config[:file_cookbook]
     end
-  end
-
-  cookbook_file "/etc/openvpn/#{config_name}-#{user_name}.conf" do
-    source "#{config_name}-#{user_name}.conf"
-    owner "root"
-    group "openvpn"
-    mode 00660
-    cookbook config[:file_cookbook] if config[:file_cookbook]
-  end
 end
 
 service "openvpn" do
